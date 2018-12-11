e3d99edabdfbd7345130871b4a6d869155452629
Monika Zietal <monika.zietal@futurum.tech>
2018-12-11 16:33:41 +0100

    Add search Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import BooksShelves from "./BooksShelves";
+import Search from "./Search";
 import * as BooksAPI from "./BooksAPI";
 
 class BooksApp extends React.Component {
@@ -12,7 +13,6 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
   };
 
   render() {
@@ -22,30 +22,7 @@ class BooksApp extends React.Component {
         <Route
           path="/search"
           render={({ history }) => (
-            <div className="search-books">
-              <div className="search-books-bar">
-                <button
-                  className="close-search"
-                  onClick={() => this.setState({ showSearchPage: false })}
-                >
-                  Close
-                </button>
-                <div className="search-books-input-wrapper">
-                  {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-                  <input type="text" placeholder="Search by title or author" />
-                </div>
-              </div>
-              <div className="search-books-results">
-                <ol className="books-grid" />
-              </div>
-            </div>
+            <Search />
           )}
         />
       </div>
