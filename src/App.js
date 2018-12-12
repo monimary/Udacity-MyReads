3df34c72dfe266486b2e3de6061bfa9ddbe2c319
Monika Zietal <monika.zietal@futurum.tech>
2018-12-12 11:38:15 +0100

    Get and display books on shelves from API

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ import * as BooksAPI from "./BooksAPI";
 
 class BooksApp extends React.Component {
   state = {
+    books: []
   };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      console.log(books);
+      this.setState({ books });
+    });
+  }
 
   render() {
     return (
       <div className="app">
-        <Route exact path="/" render={() => <BooksShelves />} />
         <Route
-          path="/search"
-          render={({ history }) => (
-            <Search />
+          exact
+          path="/"
+          render={() => (
+            <BooksShelves
+              books={this.state.books}
+            />
           )}
         />
+        <Route path="/search" render={({ history }) => <Search />} />
       </div>
     );
   }
