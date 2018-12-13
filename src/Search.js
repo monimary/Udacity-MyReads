87f81160490c06f9e255dd3e732b0d1b2d113d6a
Monika Zietal <monika.zietal@futurum.tech>
2018-12-13 15:14:27 +0100

    Add back link on search page

--- src/Search.js
+++ src/Search.js
@@ -10,12 +10,7 @@ class Search extends Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <button
-            className="close-search"
-            onClick={() => this.setState({ showSearchPage: false })}
-          >
-            Close
-          </button>
+          <Link to="/" className="close-search"></Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
